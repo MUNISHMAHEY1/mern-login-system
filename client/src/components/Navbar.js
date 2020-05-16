070f9ec3790eb37e9b83ad244123ce43a065b20c
Munish <munishmahey2@gmail.com>
2020-05-15 23:25:21 -0400

    Navbar changed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom';
 import axios from 'axios';
-// import { logoutUser } from '../actions/user_actions';
-// import { connect } from 'react-redux';
-// import { useSelector} from "react-redux";
+import { useSelector} from "react-redux";
 
 
 function Navbar (props) {
+    const user = useSelector(state => state.user)
 
     const onClickLogoutHandler = () => {
         axios.get('/api/users/logout').then(response => {
           if (response.status === 200) {
+            alert('Log Out Successful');
             props.history.push("/login");
           } else {
             alert('Log Out Failed')
@@ -21,8 +21,7 @@ function Navbar (props) {
     return(
         <nav>
             <div>
-                <Link to="/" className="brand-logo">Logo</Link>
-                        
+                <Link to="/" className="brand-logo">Logo</Link>        
                 <ul id="nav-mobile" className="right hide-on-med-and-down"> 
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
